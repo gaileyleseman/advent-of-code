f6dd7df7998bd546185cfa41f57c2786b29df1a7
gaileyleseman <gaileyleseman@gmail.com>
2020-12-04 19:51:10 +0100

    script to download user input

---
+++ dld_input.py
@@ -0,0 +1,16 @@
+import requests
+import aoc_config as cfg
+
+def download_input(year, day):
+    session = requests.Session()
+    session.get('https://adventofcode.com/')
+    headers = {'session': cfg.aoc["session-cookie"]}
+    url = "https://adventofcode.com/{0}/day/{1}/input".format(year, day)
+    filename = "./input/day{0}_input.txt".format(day)
+    page = requests.get(url, allow_redirects=True, cookies=headers)
+    open(filename, 'w').write(page.text)
+
+download_input(2020,4)
+
+
+
