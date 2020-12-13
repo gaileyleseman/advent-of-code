52b2c5971a4592bf9f0f5a05ce39af7b17d73f5f
gaileyleseman <gaileyleseman@gmail.com>
2020-12-14 00:29:49 +0100

    day 13: pt2, not fast enough for full input

--- day13.py
+++ day13.py
@@ -1,13 +1,18 @@
-#[timestamp, busses] = open('./input/day13_input.txt', 'r').read().splitlines()
-[timestamp, busses] = open('./input/day13_test.txt', 'r').read().splitlines()
+import numpy as np
+import time
+
+[timestamp, busses] = open('./input/day13_input.txt', 'r').read().splitlines()
+# [timestamp, busses] = open('./input/day13_test.txt', 'r').read().splitlines()
 timestamp = int(timestamp)
 busses = busses.split(",")
 
 # Part 1 ----------------------------------------------------------#
 available_busses = []
+i = 0
 for bus in busses:
     if bus != 'x':
         available_busses.append(int(bus))   # ignore 'x'
+    i += 1
 
 next_bus = {}
 for bus in available_busses:
@@ -19,3 +24,35 @@ ans_pt1 = (first_time - timestamp)*first_bus
 print("Part 1:", ans_pt1)
 
 # Part 2 ----------------------------------------------------------#
+first_bus = available_busses[0]
+available_busses = sorted(available_busses)
+
+time_diff = [busses.index(str(bus)) for bus in available_busses]
+time_diff = np.array(time_diff)
+time_diff = time_diff - time_diff[-1]
+
+t = time.time()
+n_busses = len(available_busses)
+step = available_busses[-1]
+min_time = 100000000000000      # surely the actual earliest timestamp wil be larger than ...
+i = min_time//step + 1
+while True:
+    timestamp = time_diff + i * step
+    checked = 0
+    for j in range(0, n_busses):
+        if not timestamp[j] % available_busses[j] == 0:
+            break
+        else:
+            checked += 1
+    if checked == n_busses:
+        idx = available_busses.index(first_bus)
+        print("Part 2:", timestamp[idx])
+        break
+    i += 1
+
+elapsed = time.time() - t
+print(elapsed)
+
+
+
+
