f7415d450cb302ba1ae82376c6340f8082d1a399
Gailey Leseman <gaileyleseman@gmail.com>
2023-12-06 23:34:00 +0100

    day 06

--- 2023/day06/day06.py
+++ 2023/day06/day06.py
@@ -1,18 +1,50 @@
 def parse_text(input_txt):
     with open(input_txt, "r") as f:
-        data = f.read().split('\n')
-    return data
+        data = f.read().split("\n")
+    times = data[0].split(": ")[1]
+    distances = data[1].split(": ")[1]
+    return times, distances
+
+
+def get_distance(button_pressed_time, race_time):
+    speed = button_pressed_time  # m/s
+    time_left = race_time - button_pressed_time
+    return speed * time_left
 
 
 def part1(data):
-    return 0
+    times, distances = data
+    times = [int(x) for x in times.split(" ") if x.strip() != ""]
+    distances = [int(x) for x in distances.split(" ") if x.strip() != ""]
+    races = list(zip(times, distances))
+
+    total = 1
+
+    for race in races:
+        count = 0
+        time, record = race
+        for i in range(time):
+            if get_distance(i, time) > record:
+                count += 1
+        total *= count
+    return total
 
 
 def part2(data):
-    return 0
+    times, distances = data
+    time = int(times.replace(" ", ""))
+    record = int(distances.replace(" ", ""))
+
+    count = 0
+
+    for i in range(time):
+        if get_distance(i, time) > record:
+            count += 1
+
+    return count
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_data = parse_text("test.txt")
     input_data = parse_text("input.txt")
 
